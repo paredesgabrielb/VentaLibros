82c76242fef149fabfade37f6aeb409301ca5ced
Gabriel Paredes <paredesgabrielb@gmail.com>
2015-11-08 16:58:54 -0300

    Primera Version sin Descuento por Tipo de pago

---
+++ VentaLibros/Articulo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaLibros
+{
+    class Articulo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public double Descuento { get; set; }
+
+        public Tipo Tipo { get; set; }
+        public Articulo(string nombre, double precio, int cantidad, Tipo tipo)
+        {
+            Nombre = nombre;
+            Precio = precio;
+            Cantidad = cantidad;
+            Tipo = tipo;
+            Descuento = getDescuentoPorTipo(tipo);
+        }
+
+        public Articulo(double precio, int cantidad, Tipo tipo)
+        {
+            Nombre = tipo.ToString();
+            Precio = precio;
+            Cantidad = cantidad;
+            Tipo = tipo;
+            Descuento = getDescuentoPorTipo(tipo);
+        }
+
+        public double getTotal()
+        {
+            return Cantidad * Precio * Descuento;
+        }
+
+        private double getDescuentoPorTipo(Tipo tipo)
+        {
+            double result = 0;
+            switch (tipo)
+            {
+                case Tipo.Cuaderno:
+                    result = 1 - 0.1;
+                    break;
+                case Tipo.Libro:
+                    result = 1 - 0.22;
+                    break;
+                case Tipo.Revista:
+                    result = 1 - 0.16;
+                    break;
+            }
+
+            return result;
+        }
+
+
+    }
+
+    enum Tipo
+    {
+        Libro,
+        Revista,
+        Cuaderno
+    }
+}
---
+++ VentaLibros/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaLibros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option = 0;
+            List<Articulo> listaArticulos = new List<Articulo>();
+
+            do
+            {
+                Console.WriteLine("**************************************");
+                Console.WriteLine("********* SISTEMA DE PEDIDOS *********");
+                Console.WriteLine("**************************************");
+                
+                Console.WriteLine("Que articulo desea comprar?");
+                Console.WriteLine("1.- Libros");
+                Console.WriteLine("2.- Revista");
+                Console.WriteLine("3.- Cuaderno");
+                Console.WriteLine("4.- Obtener Factura");
+                Console.WriteLine("5.- Salir");
+
+               option = Int32.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        listaArticulos.Add(printCantPrecio(Tipo.Libro));
+                        break;
+
+                    case 2:
+                        listaArticulos.Add(printCantPrecio(Tipo.Revista));
+                        break;
+
+                    case 3:
+                        listaArticulos.Add(printCantPrecio(Tipo.Cuaderno));
+                        break;
+                    
+                    case 4:
+                        Console.WriteLine("El Total de su Pedido es: "+listaArticulos.Sum(x=>x.getTotal()));
+                        break;
+
+                    case 5:
+                        break;
+                }
+
+            } while (option != 5);
+
+        }
+
+        static Articulo printCantPrecio(Tipo tipo)
+        {
+            int cant = 0;
+            double precio = 0;
+            Console.WriteLine("Digite Cantidad de " + tipo.ToString() + ":");
+            cant = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Digite el Precio " + tipo.ToString() + ":");
+            precio = Double.Parse(Console.ReadLine());
+            return new Articulo(precio, cant, tipo);
+        }
+    }
+}
